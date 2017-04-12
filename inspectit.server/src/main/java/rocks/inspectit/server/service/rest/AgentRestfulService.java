788935f51506781478c68e6334d872c0cdce5795
Christopher <swt79322@stud.uni-stuttgart.de>
2017-04-12 17:53:02 +0200

    changed a variable to final in order to shut the compiler up!

--- inspectit.server/src/main/java/rocks/inspectit/server/service/rest/AgentRestfulService.java
+++ inspectit.server/src/main/java/rocks/inspectit/server/service/rest/AgentRestfulService.java
@@ -10,11 +10,9 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Random;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.math3.analysis.function.Abs;
 import org.diagnoseit.standalone.Launcher;
 import org.diagnoseit.standalone.Launcher.RulePackage;
 import org.spec.research.open.xtrace.adapters.inspectit.source.InspectITTraceConverter;
@@ -33,9 +31,7 @@ import com.google.gson.JsonElement;
 
 import io.gsonfire.GsonFireBuilder;
 import io.gsonfire.TypeSelector;
-import rocks.inspectit.agent.java.sdk.opentracing.internal.impl.SpanBuilderImpl;
 import rocks.inspectit.agent.java.sdk.opentracing.internal.impl.SpanImpl;
-import rocks.inspectit.agent.java.sdk.opentracing.internal.impl.TracerImpl;
 import rocks.inspectit.agent.java.tracing.core.transformer.SpanTransformer;
 import rocks.inspectit.server.cache.IBuffer;
 import rocks.inspectit.server.cache.impl.BufferElement;
@@ -48,7 +44,6 @@ import rocks.inspectit.shared.all.communication.data.InvocationSequenceData;
 import rocks.inspectit.shared.all.communication.data.MobilePeriodicMeasurement;
 import rocks.inspectit.shared.all.tracing.data.AbstractSpan;
 import rocks.inspectit.shared.all.tracing.data.Span;
-import rocks.inspectit.shared.all.tracing.data.SpanIdent;
 import rocks.inspectit.shared.cs.cmr.service.IInvocationDataAccessService;
 import rocks.inspectit.shared.cs.cmr.service.ISpanService;
 
@@ -91,18 +86,18 @@ public class AgentRestfulService {
 
 	@RequestMapping(method = POST, value = "")
 	@ResponseBody
-	public void addNewMobileRoot(@RequestBody String json) {
+	public void addNewMobileRoot(@RequestBody final String json) {
 
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
-					
-				
+
+
 				Gson gson = getGson();
 				MobileRoot mobileRoot = gson.fromJson(json, MobileRoot.class);
 				HashMap<Long, List<AbstractSpan>> spansFromMobileRoot = new HashMap<Long, List<AbstractSpan>>();
-			
+
 				for (SpanImpl span : mobileRoot.spans) {
 					span.setTag("deviceID", mobileRoot.getDeviceID());
 					AbstractSpan abstractSpan = SpanTransformer.transformSpan(span);
@@ -110,38 +105,38 @@ public class AgentRestfulService {
 					abstractSpan.setMethodIdent(0);
 					abstractSpan.setSensorTypeIdent(0);
 					idGenerator.assignObjectAnId(abstractSpan);
-			
+
 					long traceId = abstractSpan.getSpanIdent().getTraceId();
-			
+
 					if (!spansFromMobileRoot.containsKey(traceId)) {
 						spansFromMobileRoot.put(traceId, new ArrayList<AbstractSpan>());
 					}
 					spansFromMobileRoot.get(traceId).add(abstractSpan);
-			
+
 					buffer.put(new BufferElement<DefaultData>(abstractSpan));
 				}
-			
+
 				for (MobilePeriodicMeasurement measurement : mobileRoot.measurements) {
 					measurement.setDeviceID(mobileRoot.getDeviceID());
 					measurement.setPlatformIdent(-2);
 					measurement.setSensorTypeIdent(0);
 					idGenerator.assignObjectAnId(measurement);
-			
+
 					measurement.setTimeStamp(new Timestamp(measurement.getTimestamp()));
-			
+
 					buffer.put(new BufferElement<DefaultData>(measurement));
 				}
-			
+
 				// Get all PlatformIdents
 				List<PlatformIdent> platformIdentList = new ArrayList<PlatformIdent>();
 				Collection<PlatformIdent> platformIdents = platformCache.getCleanPlatformIdents();
 				for (PlatformIdent platIdent : platformIdents) {
 					platformIdentList.add(platIdent);
 				}
-			
+
 				List<InvocationSequenceData> listSequences = dataAccessService.getInvocationSequenceOverview(0, -1, null);
 				List<InvocationSequenceData> listSequencesDetail = new LinkedList<InvocationSequenceData>();
-			
+
 				// Get all invocs
 				for (InvocationSequenceData invocationSequenceData : listSequences) {
 					InvocationSequenceData invocDetail = dataAccessService.getInvocationSequenceDetail(invocationSequenceData);
@@ -150,7 +145,7 @@ public class AgentRestfulService {
 						listSequencesDetail.add(invocDetail);
 					}
 				}
-				
+
 				if(timeoutFound(spansFromMobileRoot)){
 					try {
 						Thread.sleep(10000);
@@ -167,7 +162,7 @@ public class AgentRestfulService {
 					}
 				}
 				InspectITTraceConverter converter = new InspectITTraceConverter();
-			
+
 				// convert spans and trigger diagnoseIT
 				for (Entry<Long, List<AbstractSpan>> entry : spansFromMobileRoot.entrySet()) {
 					HashSet<Span> abstractSpans = new HashSet<Span>();
@@ -176,7 +171,7 @@ public class AgentRestfulService {
 					Trace trace = converter.convertTraces(listSequencesDetail, platformIdentList,
 							new ArrayList<Span>(abstractSpans), mobileRoot.measurements);
 					Launcher.startLauncher(trace, RulePackage.MobilePackage);
-				}		
+				}
 			}
 		}).start();
 	}
@@ -185,7 +180,7 @@ public class AgentRestfulService {
 		for (Entry<Long, List<AbstractSpan>> entry : spansFromMobileRoot.entrySet()) {
 			for(AbstractSpan span: entry.getValue()){
 				String timeout = span.getTags().get("http.request.timeout");
-				if(timeout != null && timeout.equals("true")){
+				if((timeout != null) && timeout.equals("true")){
 					return true;
 				}
 			}
@@ -197,21 +192,21 @@ public class AgentRestfulService {
 		GsonFireBuilder builder = new GsonFireBuilder().registerTypeSelector(DefaultData.class,
 				new TypeSelector<DefaultData>() {
 
-					@Override
-					public Class<? extends DefaultData> getClassForElement(JsonElement readElement) {
-						String type = readElement.getAsJsonObject().get("type").getAsString();
+			@Override
+			public Class<? extends DefaultData> getClassForElement(JsonElement readElement) {
+				String type = readElement.getAsJsonObject().get("type").getAsString();
 
-						if (type.equals("MobilePeriodicMeasurement")) {
-							return MobilePeriodicMeasurement.class;
-						} else {
-							/*
-							 * returning null will trigger Gson's default
-							 * behavior
-							 */
-							return null;
-						}
-					}
-				});
+				if (type.equals("MobilePeriodicMeasurement")) {
+					return MobilePeriodicMeasurement.class;
+				} else {
+					/*
+					 * returning null will trigger Gson's default
+					 * behavior
+					 */
+					return null;
+				}
+			}
+		});
 		Gson gson = builder.createGson();
 		return gson;
 	}
